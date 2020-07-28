7b105f71270125dc30a3685b51c802276730ebda
Teody C. Seguin <teodyseguin@gmail.com>
2020-07-28 16:11:55 +0800

    Add command to install custom plugins

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -2,3 +2,13 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install some custom plugins
+
+cd ~/.oh-my-zsh/custom/plugins
+# Syntax Highlighting
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git
+# Auto suggestions
+git clone git@github.com:zsh-users/zsh-autosuggestions.git
+# Completions
+git clone git@github.com:zsh-users/zsh-completions.git
+
