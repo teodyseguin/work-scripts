3d8a6b12daf49ec2c4ddb362c66978c9ce2e8028
Teody C. Seguin <teodyseguin@gmail.com>
2021-07-03 15:43:11 +0800

    create nvm install script

---
+++ nvm-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Install NVM
+# Check out https://github.com/nvm-sh/nvm/releases for latest release version.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
